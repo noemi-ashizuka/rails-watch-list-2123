16f75a723f2e860961fefff8dcb65bb1f3f3038d
noemi ashizuka <noemi.calanducci@gmail.com>
2025-11-13 12:14:34 +0900

    initial setup

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
+
+
+# User stories
+
+# As a user, I can see all my movie lists
+# As a user, I can create a movie list
+# As a user, I can see the details of a movie list
+# As a user, I can bookmark a movie inside a movie list
+# As a user, I can destroy a bookmark
